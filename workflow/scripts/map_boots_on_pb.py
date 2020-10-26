1efc5c6e223ccdf364cfcecc4295fa7f80edcb5a
Lael Barlow <lael@ualberta.ca>
2020-10-26 10:50:18 -0600

    Add function for checking taxon names match between input trees for the map_boots_on_pb script.

--- workflow/scripts/map_boots_on_pb.py
+++ workflow/scripts/map_boots_on_pb.py
@@ -33,6 +33,43 @@ def reformat_combined_supports(tree_string):
     return tree_string
 
 
+def check_leaf_names_match(t1, t2):
+    """Take two ETE3 TreeNode objects and check that the lists of leaf names
+    are perfectly overlapping sets.
+    """
+    # Get leaf names from tree 1.
+    t1_names = [leaf.name for leaf in t1.get_leaves()]
+
+    # Get leaf names from tree 2.
+    t2_names = [leaf.name for leaf in t2.get_leaves()]
+
+    # Find leaf names in tree 1 but not 2.
+    in_1_not_2 = []
+    for i in t1_names:
+        if not i in t2_names:
+            in_1_not_2.append(i)
+    if len(in_1_not_2) > 0:
+        print("""The following leaf names are in tree 1 but not tree 2:
+        %s""" % ',\n'.join(in_1_not_2))
+
+    # Find leaf names in tree 2 but not 1.
+    in_2_not_1 = []
+    for i in t2_names:
+        if not i in t1_names:
+            in_2_not_1.append(i)
+    if len(in_2_not_1) > 0:
+        print("""The following leaf names are in tree 2 but not tree 1:
+        %s""" % ',\n'.join(in_2_not_1))
+
+    # Assert that both trees have the same number of leaf nodes.
+    assert len(t1_names) == len(t2_names), """Different numbers of terminal
+    (leaf) nodes in input trees."""
+
+    # Assert they are identical sets.
+    assert len(list(set(t1_names + t2_names))) == len(t1_names), """Sets of
+    terminal (leaf) node names do not perfectly overlap."""
+
+
 def combine_supports(boot_newick, prob_newick, combined_figtree_newick):
     """Takes two newick files and writes another newick file that when opened
     in figtree will show posterior probabilities and bootstrap values together.
@@ -47,6 +84,9 @@ def combine_supports(boot_newick, prob_newick, combined_figtree_newick):
     # Parse the input newick tree files.
     boot_newick_tree = Tree(boot_newick)
     prob_newick_tree = Tree(prob_newick)
+
+    # Check that the leaf node names match.
+    check_leaf_names_match(boot_newick_tree, prob_newick_tree)
 
     # Root trees on the same node arbitrarily.
     arbitrary_leaf_node = prob_newick_tree.get_leaves()[0]
