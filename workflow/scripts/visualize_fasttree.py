1d3b704308a37fb02315890a6c0feee2ef7096bb
Lael Barlow <lael@ualberta.ca>
2020-10-29 11:32:52 -0600

    Add visualize_boots_on_pb script for parsing branch supports properly during re-rooting.

--- workflow/scripts/visualize_fasttree.py
+++ workflow/scripts/visualize_fasttree.py
@@ -68,75 +68,6 @@ def customize_node_styles_for_visualization(t):
            n.add_face(name_face, column=0, position='branch-right')
 
 
-def lighten_color(color, amount=0.5):
-    """Takes a hex colour code and returns a hex code for the same colour but
-    lighter by multiplying (1-luminosity) by the given amount (fraction).
-    """
-    try:
-        c = mc.cnames[color]
-    except:
-        c = color
-    c = colorsys.rgb_to_hls(*mc.to_rgb(c))
-    ct = colorsys.hls_to_rgb(c[0], 1 - amount * (1 - c[1]), c[2])
-    return '#%02x%02x%02x' % (int(ct[0]*255), int(ct[1]*255), int(ct[2]*255))
-
-
-def highlight_node(t, node):
-    """Take an ete3 tree object and a list of nodes with paralogues, and modify
-    the node styles to emphasize important features.
-    """
-    # Make a list of colours as an iterable.
-    # Colour scheme source: https://www.nature.com/articles/nmeth.1618?WT.ec_id=NMETH-201106
-    colour_iterable = iter(['#%02x%02x%02x' % (230, 159, 0),#Orange
-                            '#%02x%02x%02x' % (86, 180, 233),#Sky blue
-                            '#%02x%02x%02x' % (0, 158, 115),#Bluish green
-                            '#%02x%02x%02x' % (240, 228, 66),#Yellow
-                            '#%02x%02x%02x' % (0, 114, 178),#Blue
-                            '#%02x%02x%02x' % (213, 94, 0),#Vermillion
-                            '#%02x%02x%02x' % (204, 121, 167)#Reddish purple
-                            ]*100)
-
-    # Get current highlight colour, if any.
-    current_bgcolor = node.img_style['bgcolor']
-
-    # Set up a node style for clades with paralogues.
-    nstyle2 = NodeStyle()
-    nstyle2["shape"] = "sphere"
-    nstyle2["size"] = 0
-    nstyle2["vt_line_color"] = "#000000"
-    nstyle2["hz_line_color"] = "#000000"
-    nstyle2["vt_line_width"] = 2
-    nstyle2["hz_line_width"] = 2
-    nstyle2["vt_line_type"] = 0 # 0 solid, 1 dashed, 2 dotted
-    nstyle2["hz_line_type"] = 0
-
-    # Make sure that the new colour is different than the old color (so that
-    # all highlighted clades are actually visible.
-    same_bgcolor = True
-    new_bgcolor = None
-    while same_bgcolor:
-        new_bgcolor = lighten_color(next(colour_iterable), 0.5)
-        if new_bgcolor != current_bgcolor:
-            same_bgcolor = False
-    assert new_bgcolor != current_bgcolor
-
-    # Set new background color.
-    nstyle2["bgcolor"] = new_bgcolor
-
-    ## Temp.
-    #print('\n\nNode:')
-    #print(node)
-    #print('current_bgcolor')
-    #print(current_bgcolor)
-    #print('new_bgcolor')
-    #print(new_bgcolor)
-
-    # Set the node style for node and all subnodes.
-    node.set_style(nstyle2)
-    for sn in node.traverse():
-        sn.set_style(nstyle2)
-
-
 if __name__ == '__main__':
 
     # Parse command line arguments.
@@ -172,16 +103,16 @@ if __name__ == '__main__':
 
     # Write tree to pdf.
 
-    # Use this for running on personal computer:
-    t2.render(output_file_path, tree_style=ts, w=8.5, h=11, units='in', dpi=600)
+    ## Use this for running on personal computer:
+    #t2.render(output_file_path, tree_style=ts, w=8.5, h=11, units='in', dpi=600)
     
-    ## Use this for running on a cluster:
-    #vdisplay = Xvfb()
-    #vdisplay.start()
-    #try:
-    #    t2.render(output_file_path, tree_style=ts, w=8.5, h=11, units='in', dpi=600)
-    #finally:
-    #    vdisplay.stop()
+    # Use this for running on a cluster:
+    vdisplay = Xvfb()
+    vdisplay.start()
+    try:
+        t2.render(output_file_path, tree_style=ts, w=8.5, h=11, units='in', dpi=600)
+    finally:
+        vdisplay.stop()
 
 
     #####################################################
