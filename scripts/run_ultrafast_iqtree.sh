40dc116ee55ee350ac4dc5697ee12e2438fdb82b
Lael Barlow <lael@ualberta.ca>
2020-11-19 15:15:09 -0700

    Update run scripts.

--- scripts/run_ultrafast_iqtree.sh
+++ scripts/run_ultrafast_iqtree.sh
@@ -3,11 +3,20 @@
 # Determine what Snakemake profile to use.
 source scripts/determine_snakemake_profile.sh
 
+# Determine whether to use the --profile option at all.
+profile_option="--profile"
+if [ "$snakemake_profile" == "" ]; then
+    profile_option="--cores 1"
+fi
+
+# Determine which environment management options to use.
+source scripts/determine_snakemake_env_options.sh
+
 # Activate python virtual environment.
 source scripts/workflow_python_env_definition.sh
 
 # Run snakemake in python virtual environment.
-snakemake ultrafast_iqtree -j 100 --use-conda --profile $snakemake_profile
+snakemake ultrafast_iqtree -j 100 $env_options $profile_option $snakemake_profile
 
 # Deactivate python virtual environment.
 source scripts/deactivate_python_env.sh
