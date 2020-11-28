74c78c2acca04783d21b5680272c88d46ab3c3f9
Lael Barlow <lael@ualberta.ca>
2020-11-27 18:53:27 -0700

    Test envmodules with cdhit_subsets rule.

--- resources/example_slurm-jobscript.sh
+++ resources/example_slurm-jobscript.sh
@@ -1,17 +1,36 @@
 #!/bin/bash
 # properties = {properties}
 
-# Load modules.
-module load  \
-cd-hit/4.8.1 \
-fasttree/2.1.10 \
-iq-tree/2.0.7 \
-mafft/7.471 \
-nixpkgs/16.09 \
-intel/2018.3 \
-trimal/1.4 \
-openmpi/3.1.4 \
-phylobayes-mpi/20180420 
+#### Load modules.
+###module load  \
+###
+### 
+####nixpkgs/16.09  gcc/7.3.0
+####nixpkgs/16.09  intel/2018.3
+###
+###cd-hit/4.8.1 \
+###fasttree/2.1.10 \
+###
+####       StdEnv/2020  gcc/9.3.0
+####      StdEnv/2020  intel/2020.1.217
+###
+###iq-tree/2.0.7 \
+###
+#### StdEnv/2020
+###
+###mafft/7.471 \
+###
+#### StdEnv/2020
+####       nixpkgs/16.09  gcc/7.3.0
+####      nixpkgs/16.09  intel/2018.3
+###
+###trimal/1.4 \
+###
+####      nixpkgs/16.09  intel/2018.3  openmpi/3.1.2
+####      nixpkgs/16.09  intel/2018.3  openmpi/3.1.4
+###
+###phylobayes-mpi/20180420 
+
 
 # Execute job.
 {exec_job}
